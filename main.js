f509745c857ff18664cb037f42494b3ee91615ad
albinolssonn <56583621+albinolssonn@users.noreply.github.com>
2021-07-26 21:11:14 +0200

    Adding all functions to the gallery

---
+++ main.js
@@ -0,0 +1,17 @@
+var id1 = "hover-1"
+var id2 = "hover-2"
+var id3 = "hover-3"
+var id4 = "hover-4"
+var id5 = "hover-5"
+var id6 = "hover-6"
+
+
+function mouseOver(tmpId) {
+    document.getElementById(tmpId).style.display = "block";
+    
+}
+
+function mouseOut(tmpId) {
+    document.getElementById(tmpId).style.display = "none";
+}
+    
