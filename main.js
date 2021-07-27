3667f8c3e7f85664779b75b2ee4593d5699fe1c3
albinolssonn <56583621+albinolssonn@users.noreply.github.com>
2021-07-27 15:04:18 +0200

    Fixed Contact functions

--- main.js
+++ main.js
@@ -4,14 +4,29 @@ var id3 = "hover-3"
 var id4 = "hover-4"
 var id5 = "hover-5"
 var id6 = "hover-6"
+var containerMain = "containerMain"
+var containerContact = "containerContact"
 
 
 function mouseOver(tmpId) {
-    document.getElementById(tmpId).style.display = "block";
+    document.getElementById(tmpId).style.opacity = "100%";
     
 }
 
 function mouseOut(tmpId) {
-    document.getElementById(tmpId).style.display = "none";
+    document.getElementById(tmpId).style.opacity = "0%";
 }
+
+
+function onClickContact(containerMain, containerContact) {
+    document.getElementById(containerMain).style.opacity = "0%";
+    document.getElementById(containerContact).style.opacity = "100%";
+    document.getElementById("contactBtn").style.opacity = "0%";
     
+}
+
+function onQuitContact(container) {
+    document.getElementById(containerMain).style.opacity = "100%";
+    document.getElementById(containerContact).style.opacity = "0%";
+    document.getElementById("contactBtn").style.opacity = "100%";
+}
