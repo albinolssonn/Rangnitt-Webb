cb3a90b0ba28b980aae6a4b34cd1ee127042b892
albinolssonn <56583621+albinolssonn@users.noreply.github.com>
2021-07-29 17:11:59 +0200

    Adding images and fixed bug with slider/contact

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var id3 = "hover-3"
 var id4 = "hover-4"
 var id5 = "hover-5"
 var id6 = "hover-6"
+var id7 = "hover-7"
+
 var containerMain = "containerMain"
 var containerContact = "containerContact"
 
@@ -19,10 +21,12 @@ function onClickContact(containerMain, containerContact) {
     document.getElementById(containerMain).style.opacity = "0%";
     document.getElementById(containerContact).style.opacity = "100%";
     document.getElementById("contactBtn").style.opacity = "0%"; 
+    document.getElementById(containerContact).style.zIndex = "999"; 
 }
 
 function onQuitContact(container) {
     document.getElementById(containerMain).style.opacity = "100%";
     document.getElementById(containerContact).style.opacity = "0%";
     document.getElementById("contactBtn").style.opacity = "100%";
+    document.getElementById(containerContact).style.zIndex = "-1"; 
 }
