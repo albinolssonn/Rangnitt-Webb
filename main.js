ca1e172346ba8a73d5abb4d36102adcaadb245f7
albinolssonn <56583621+albinolssonn@users.noreply.github.com>
2021-07-30 20:59:30 +0200

    Bug fix

--- main.js
+++ main.js
@@ -11,22 +11,28 @@ var containerContact = "containerContact"
 
 function mouseOver(tmpId) {
     document.getElementById(tmpId).style.opacity = "100%";
+    document.getElementById(tmpId).style.zIndex = "3";
+    console.log("1");
 }
 
 function mouseOut(tmpId) {
     document.getElementById(tmpId).style.opacity = "0%";
+    document.getElementById(tmpId).style.zIndex = "-3";
+    console.log("2");
 }
 
-function onClickContact(containerMain, containerContact) {
+function onClickContact() {
     document.getElementById(containerMain).style.opacity = "0%";
     document.getElementById(containerContact).style.opacity = "100%";
     document.getElementById("contactBtn").style.opacity = "0%"; 
     document.getElementById(containerContact).style.zIndex = "999"; 
+    console.log("3");
 }
 
-function onQuitContact(container) {
+function onQuitContact() {
     document.getElementById(containerMain).style.opacity = "100%";
     document.getElementById(containerContact).style.opacity = "0%";
     document.getElementById("contactBtn").style.opacity = "100%";
     document.getElementById(containerContact).style.zIndex = "-1"; 
+    console.log("4");
 }
